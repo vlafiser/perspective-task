fb7ac6011bfcddcde000776e6e1c0c7f0ec89f22
Vlastimil Fiser <email@vlastimilfiser.com>
2024-12-04 04:05:54 +0100

    Fix of wrong animation on the previous button + correct position of the buttons

--- app/components/funnel/Navigation.tsx
+++ app/components/funnel/Navigation.tsx
@@ -16,18 +16,18 @@ const FunnelNavigation: React.FC<FunnelNavigationProps> = ({
   onNext,
 }) => {
 
-  const iconClassNames = "w-8 h-8 text-blue-500 relative opacity-100 group-active:translate-x-1 transition-transform duration-150 group-disabled:translate-x-0 group-disabled:opacity-60 will-change-transform"
+  const iconClassNames = "w-8 h-8 text-blue-500 relative opacity-100 transition-transform duration-150 group-disabled:translate-x-0 group-disabled:opacity-60 will-change-transform"
 
   const buttons = [
     {
       onClick: onPrevious,
-      position: 'left-[419px]',
+      position: 'left-[409px]',
       disabledCondition: currentPage === 0,
       icon: <ArrowLeftIcon className={iconClassNames + ' group-active:-translate-x-1'} />
     },
     {
       onClick: onNext,
-      position: 'right-[419px]',
+      position: 'right-[409px]',
       disabledCondition: currentPage >= totalPages - 1,
       icon: <ArrowRightIcon className={iconClassNames + ' group-active:translate-x-1'} />
     }
